5067827fcf20e2afd89aeb816ad99b0c3362dd5c
Jimmy Yumeng Sun <jimmy.sun@fredsinc.com>
2017-10-14 12:44:38 -0500

    track changes

---
+++ src/main/java/com/boot/config/PresistenceConfiguration.java
@@ -0,0 +1,18 @@
+package com.boot.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PresistenceConfiguration {
+
+	@Bean
+	@ConfigurationProperties(prefix="spring.datasource")
+	public DataSource dataSource() {
+		return DataSourceBuilder.create().build();
+	}
+}
